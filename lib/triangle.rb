d221bf6937ff61dd0fd02aa29b6f5e098ed5b857
James Mobus <mobusj@live.com>
2019-09-26 19:21:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
     @side2 = side2
     @side3 = side3
     
-    if side1 == 0 or side2 == 0 or side3 == 0
+    if side1 == 0 || side2 == 0 || side3 == 0
       begin
         raise TriangleError
       rescue TriangleError => error
